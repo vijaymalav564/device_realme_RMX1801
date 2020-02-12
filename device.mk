01c2904bef1ef93542b44490378dc6e9f43c5ae1
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    RMX1801: Enable freeform windows

--- device.mk
+++ device.mk
@@ -223,6 +223,10 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.realme_sdm660 \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
